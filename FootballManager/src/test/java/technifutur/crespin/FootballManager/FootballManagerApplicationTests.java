8cab6632d7631da7351cf5ba48456556620f100a
Crespin <crespin.christophe@netc.eu>
2022-04-19 11:10:04 +0200

    First commit

---
+++ FootballManager/src/test/java/technifutur/crespin/FootballManager/FootballManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package technifutur.crespin.FootballManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
