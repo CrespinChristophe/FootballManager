065cc1213a2bceabbf5eb083cc6f8c1f52799ff6
Crespin <crespin.christophe@netc.eu>
2022-04-21 11:03:59 +0200

    Dernière finalisation des forms et DTO

--- FootballManager/src/main/java/technifutur/crespin/FootballManager/model/dto/JoueurDTO.java
+++ FootballManager/src/main/java/technifutur/crespin/FootballManager/model/dto/JoueurDTO.java
@@ -1,5 +1,6 @@
 package technifutur.crespin.FootballManager.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,5 +11,20 @@ public class JoueurDTO {
     private int id;
     private String nom;
     private String prenom;
+    private ClubDTO clubDTO;
+    private ManagerDTO managerDTO;
+
+    @Data
+    @AllArgsConstructor
+    private static class ClubDTO {
+        private String nom;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class ManagerDTO {
+        private String nom;
+        private String prenom;
+    }
 
 }
